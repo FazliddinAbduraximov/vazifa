fd220dda8ec07b9a3aa5925ab868946f4065ac0c
Fazliddin <fazliddin.abdurahimov9602@gmail.com>
2025-11-02 18:06:03 +0500

    route2

--- src/pages/home/components/audio.jsx
+++ src/pages/home/components/audio.jsx
@@ -3,10 +3,10 @@ import { AUDIO } from "../../../data/audio"
 
 export const AudioProduct=()=>{
     return (
-        <div>
+        <div className="mb-[93px] mt-16">
             <div className="container">
                 <div>
-                    <h1 className="font-semibold text-[32px]">Янги қўшилганлар</h1>
+                    <h1 className="font-semibold text-[32px]">Аудио китоблар</h1>
                     <div className="mt-8 mb-8 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6">
                         {
                             AUDIO.map((e)=>(
