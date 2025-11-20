b65a8b65fa79fca2201934ce79305fed8949d77c
Fazliddin <fazliddin.abdurahimov9602@gmail.com>
2025-11-20 06:05:21 +0500

    ui shadcn

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { ShadcnExcemples } from "./components/shadcn-excemples"
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <ShadcnExcemples />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/shadcn-excemples.tsx
@@ -0,0 +1,11 @@
+import { Button } from './ui/button'
+
+export const ShadcnExcemples = () => {
+    return (
+        <>
+            <div className="py-[25px]">
+                <Button className="px-[50px]">Send</Button>
+            </div>
+        </>
+    )
+}
